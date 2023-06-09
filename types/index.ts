09449fbdeaf38c7fec51ce885c89fe112b49ee76
Brenno <brennom@gmail.com>
2023-06-09 16:56:57 -0400

    feat:combobox filteredmanufacturers.function

--- types/index.ts
+++ types/index.ts
@@ -6,3 +6,8 @@ export interface CustomButtonProps{
     handleClick?: MouseEventHandler<HTMLButtonElement>;
     btnType?: "button" | "Submit";
 }
+
+export interface SearchManufacturerProps{
+    manufacturer: string;
+    setManufacturer:(manufacturer: string) => void;
+}
